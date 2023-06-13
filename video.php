be9c93c1cd906419d1bb8a45bc22072936e40a63
Christian Yikwa <136363082+Christianjikwa@users.noreply.github.com>
2023-06-13 20:10:19 +0700

    first commit

---
+++ video.php
@@ -0,0 +1,81 @@
+<?php include'header.php'; ?>
+<head>
+<link rel="stylesheet" type="text/css" href="styles/services.css">
+<link rel="stylesheet" type="text/css" href="styles/services_responsive.css">
+	<title>Daftar Video - GIDi</title>
+</head>
+
+	<div class="home">
+		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="img/gambar/efata.jpeg"  data-speed="0.8"></div>
+		<div class="home_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="home_content">
+							<div class="home_title"><span>VIDEO</span> GIDI</div>
+							<div class="breadcrumbs">
+								<ul>
+									<li><a href="index.php">Beranda</a></li>
+									<li>Video</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Services -->
+
+
+	<div class="services">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title"><h2>Daftar Video Jemaat GIDI Cyklop Sentani</h2></div>
+					
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+
+
+<div class="container">
+  	<div class="left">
+ 		 <iframe width="560" height="315" src="https://www.youtube.com/embed/zobZ59_7_Xg" frameborder="0" allowfullscreen></iframe>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/Ui0oftMC0Lg" frameborder="0" allowfullscreen></iframe>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/ROICNFt9Dv8" frameborder="0" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Ni6msMblOvQ" frameborder="0" allowfullscreen></iframe>
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/1K5U7iuVkxs" frameborder="0" allowfullscreen></iframe>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/-AdXWdysAfo" frameborder="0" allowfullscreen></iframe>
+  </div>
+
+  <div class="right">
+  		<iframe width="560" height="315" src="https://www.youtube.com/embed/K2zjCw-8mKE" frameborder="0" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/oJQVAwkDCnM" frameborder="0" allowfullscreen></iframe>	
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/zARC7JtuhHs" frameborder="0" allowfullscreen></iframe>			
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/nq2bzOu9EQ0" frameborder="0" allowfullscreen></iframe>			
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/YDChZu9UjLY" frameborder="0" allowfullscreen></iframe>			
+
+  	</div>
+</div>
+
+<style>
+	.container {
+  display: flex;
+}
+
+.left, .right {
+  width: 50%;
+  padding: 0 10px;
+}
+
+</style>
+
+
+	
+
+<?php include'footer.php'; ?>
