86dc1c93ade7a50e9c1860bdcbdb6a55f5202260
Christian <136363082+Christianjikwa@users.noreply.github.com>
2023-06-26 15:29:31 +0700

    Update video.php

--- video.php
+++ video.php
@@ -28,53 +28,39 @@
 
 	<!-- Services -->
 
-
 	<div class="services">
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<div class="section_title"><h2>Daftar Video Jemaat GIDI Cyklop Sentani</h2></div>
-					
+					<div class="section_title"><h2>Daftar Video</h2></div>
 				</div>
 			</div>
 
 
+			<div class="row services_row">
+
+
+								 <?php
+				            $qry = mysqli_query($konek,"SELECT * FROM tbl_video where kode order by kode limit 100");
+				            while ($data=mysqli_fetch_assoc($qry)) {
+				          ?>
+				<div class="col-lg-6 col-md-6 service_col">
+					<div class="service_title trans_200"><?php echo $data['nama']; ?></div>
+							<iframe width="100%" height="290" src="https://www.youtube.com/embed/<?php echo $data['alamat']; ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
+
+				</div>
+
+
+				
+			<?php } ?>
+			</div>
+
+
+
 		</div>
 	</div>
-
-
-<div class="container">
-  	<div class="left">
- 		 <iframe width="560" height="315" src="https://www.youtube.com/embed/zobZ59_7_Xg" frameborder="0" allowfullscreen></iframe>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/Ui0oftMC0Lg" frameborder="0" allowfullscreen></iframe>
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/ROICNFt9Dv8" frameborder="0" allowfullscreen></iframe>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/Ni6msMblOvQ" frameborder="0" allowfullscreen></iframe>
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/1K5U7iuVkxs" frameborder="0" allowfullscreen></iframe>
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/-AdXWdysAfo" frameborder="0" allowfullscreen></iframe>
-  </div>
-
-  <div class="right">
-  		<iframe width="560" height="315" src="https://www.youtube.com/embed/K2zjCw-8mKE" frameborder="0" allowfullscreen></iframe>
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/oJQVAwkDCnM" frameborder="0" allowfullscreen></iframe>	
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/zARC7JtuhHs" frameborder="0" allowfullscreen></iframe>			
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/nq2bzOu9EQ0" frameborder="0" allowfullscreen></iframe>			
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/YDChZu9UjLY" frameborder="0" allowfullscreen></iframe>			
-
-  	</div>
-</div>
-
-<style>
-	.container {
-  display: flex;
-}
-
-.left, .right {
-  width: 50%;
-  padding: 0 10px;
-}
-
-</style>
-
 
 	
 
